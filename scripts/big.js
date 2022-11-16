3ad4c18060f4f740358e6fa48b8fd3f32612833f
SuperBX <90575765+SuperBX@users.noreply.github.com>
2022-11-16 17:04:32 -0500

    fix some error and gallery

---
+++ scripts/big.js
@@ -0,0 +1,12 @@
+function
+
+bigImg(x) {
+    x.style.height = "300px";
+    x.style.width = "300px";
+ }
+
+ function normalImg(x) {
+    x.style.height = "210px";
+    x.style.width = "210px";
+ }
+
