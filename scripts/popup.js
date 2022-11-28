6684bcc20e610aa883b46aadbe9a0838b8f5a495
Ming DeMers <mtd64@cornell.edu>
2022-11-28 18:45:44 -0500

    pop up only shows once a day

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,12 @@
+$(document).ready(function () {
+    if ($.cookie('popup') = 'seen') {
+        $("dim-sum").addClass('hidden');
+    }
+    else {
+        $.cookie('popup', 'seen', { expires: 1 });
+    }
+});
 
-$('#confirm').click(function() {
-    $("#dim-sum").addClass("hidden")
+$('#confirm').click(function () {
+    $("#dim-sum").addClass("hidden");
 });
