e6590cfb7b2079da0f05802bd878361ff2ec4bf2
Ming DeMers <mtd64@cornell.edu>
2022-11-30 11:55:50 -0500

    improved membership FAQs - Questions can now close and open - No box outline

--- scripts/faq.js
+++ scripts/faq.js
@@ -1,19 +1,5 @@
-$('membership.html').ready(function() {
+$('membership.html').ready(function () {
     $("#ans1").addClass("hidden")
-    $("#ans2").addClass("hidden")
-    $("#ans3").addClass("hidden")
-    $("#ans4").addClass("hidden")
-    $("#ans5").addClass("hidden")
-    $("#ans6").addClass("hidden")
-    $("#ans7").addClass("hidden")
-    $("#ans8").addClass("hidden")
-    $("#ans9").addClass("hidden")
-    $("#ans10").addClass("hidden")
-    $("#ans11").addClass("hidden")
-  });
-
-  $('#q1').click(function() {
-    $("#ans1").removeClass("hidden")
     $("#ans2").addClass("hidden")
     $("#ans3").addClass("hidden")
     $("#ans4").addClass("hidden")
@@ -26,8 +12,22 @@ $('membership.html').ready(function() {
     $("#ans11").addClass("hidden")
 });
 
-$('#q2').click(function() {
-    $("#ans2").removeClass("hidden")
+$('#q1').click(function () {
+    $("#ans1").toggleClass("hidden")
+    $("#ans2").addClass("hidden")
+    $("#ans3").addClass("hidden")
+    $("#ans4").addClass("hidden")
+    $("#ans5").addClass("hidden")
+    $("#ans6").addClass("hidden")
+    $("#ans7").addClass("hidden")
+    $("#ans8").addClass("hidden")
+    $("#ans9").addClass("hidden")
+    $("#ans10").addClass("hidden")
+    $("#ans11").addClass("hidden")
+});
+
+$('#q2').click(function () {
+    $("#ans2").toggleClass("hidden")
     $("#ans1").addClass("hidden")
     $("#ans3").addClass("hidden")
     $("#ans4").addClass("hidden")
@@ -40,8 +40,8 @@ $('#q2').click(function() {
     $("#ans11").addClass("hidden")
 });
 
-$('#q3').click(function() {
-    $("#ans3").removeClass("hidden")
+$('#q3').click(function () {
+    $("#ans3").toggleClass("hidden")
     $("#ans2").addClass("hidden")
     $("#ans1").addClass("hidden")
     $("#ans4").addClass("hidden")
@@ -54,8 +54,8 @@ $('#q3').click(function() {
     $("#ans11").addClass("hidden")
 });
 
-$('#q4').click(function() {
-    $("#ans4").removeClass("hidden")
+$('#q4').click(function () {
+    $("#ans4").toggleClass("hidden")
     $("#ans2").addClass("hidden")
     $("#ans3").addClass("hidden")
     $("#ans1").addClass("hidden")
@@ -68,8 +68,8 @@ $('#q4').click(function() {
     $("#ans11").addClass("hidden")
 });
 
-$('#q5').click(function() {
-    $("#ans5").removeClass("hidden")
+$('#q5').click(function () {
+    $("#ans5").toggleClass("hidden")
     $("#ans2").addClass("hidden")
     $("#ans3").addClass("hidden")
     $("#ans4").addClass("hidden")
@@ -82,8 +82,8 @@ $('#q5').click(function() {
     $("#ans11").addClass("hidden")
 });
 
-$('#q6').click(function() {
-    $("#ans6").removeClass("hidden")
+$('#q6').click(function () {
+    $("#ans6").toggleClass("hidden")
     $("#ans2").addClass("hidden")
     $("#ans3").addClass("hidden")
     $("#ans4").addClass("hidden")
@@ -96,8 +96,8 @@ $('#q6').click(function() {
     $("#ans11").addClass("hidden")
 });
 
-$('#q7').click(function() {
-    $("#ans7").removeClass("hidden")
+$('#q7').click(function () {
+    $("#ans7").toggleClass("hidden")
     $("#ans2").addClass("hidden")
     $("#ans3").addClass("hidden")
     $("#ans4").addClass("hidden")
@@ -110,8 +110,8 @@ $('#q7').click(function() {
     $("#ans11").addClass("hidden")
 });
 
-$('#q8').click(function() {
-    $("#ans8").removeClass("hidden")
+$('#q8').click(function () {
+    $("#ans8").toggleClass("hidden")
     $("#ans2").addClass("hidden")
     $("#ans3").addClass("hidden")
     $("#ans4").addClass("hidden")
@@ -124,8 +124,8 @@ $('#q8').click(function() {
     $("#ans11").addClass("hidden")
 });
 
-$('#q9').click(function() {
-    $("#ans9").removeClass("hidden")
+$('#q9').click(function () {
+    $("#ans9").toggleClass("hidden")
     $("#ans2").addClass("hidden")
     $("#ans3").addClass("hidden")
     $("#ans4").addClass("hidden")
@@ -138,8 +138,8 @@ $('#q9').click(function() {
     $("#ans11").addClass("hidden")
 });
 
-$('#q10').click(function() {
-    $("#ans10").removeClass("hidden")
+$('#q10').click(function () {
+    $("#ans10").toggleClass("hidden")
     $("#ans2").addClass("hidden")
     $("#ans3").addClass("hidden")
     $("#ans4").addClass("hidden")
@@ -152,8 +152,8 @@ $('#q10').click(function() {
     $("#ans11").addClass("hidden")
 });
 
-$('#q11').click(function() {
-    $("#ans11").removeClass("hidden")
+$('#q11').click(function () {
+    $("#ans11").toggleClass("hidden")
     $("#ans2").addClass("hidden")
     $("#ans3").addClass("hidden")
     $("#ans4").addClass("hidden")
